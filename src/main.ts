4c1df8104462acaf60c176927c959ea381e3d7c2
Daniel Iovescu <iovescu.dani@gmail.com>
2023-10-20 20:03:07 +0300

    added token load and bearer token attachment

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-import router from "./router";
 import { createPinia } from "pinia";
 import { useAuthStore } from "./stores/auth.store";
 import { StorageKeys } from "./utils/consts";
 
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const pinia = createPinia();
 const app = createApp(App).use(router).use(pinia);
-const auth = useAuthStore();
 
+const auth = useAuthStore();
 const token = localStorage.getItem(StorageKeys.TOKEN);
 if (token) {
   auth.setToken(token);
--- src/utils/http.service.ts
+++ src/utils/http.service.ts
@@ -1,15 +1,28 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { useAuthStore } from "@/stores/auth.store";
+import router from "@/router";
+import { StorageKeys } from "./consts";
 
 const httpService = axios.create();
 
 httpService.interceptors.request.use((config) => {
   console.log("Imcomming request...");
+  const token = localStorage.getItem(StorageKeys.TOKEN);
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
-httpService.interceptors.response.use((config) => {
-  console.log("Imcomming response...");
-  return config;
-});
+httpService.interceptors.response.use(
+  (config) => config,
+  (err: AxiosError) => {
+    const status = err.response?.status;
+    if (status === 401 || status === 403) {
+      useAuthStore().logOut();
+      router.replace("/auth");
+    }
+  }
+);
 
 export default httpService;
